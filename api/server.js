105435ea10f5cf72305bd2ca1a3e81b6a9546b79
Alexandr <vashchekin01@mail.ru>
2023-12-16 15:26:52 +0300

    fix: bug fixs - 27

--- api/server.js
+++ api/server.js
@@ -16,13 +16,13 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
+server.use(jsonServer.bodyParser);
 
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
 server.use(router);
