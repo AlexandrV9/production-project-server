89608db52e67cc19f66814836a4cd00a83c1a9c7
Alexandr <vashchekin01@mail.ru>
2023-12-16 14:37:17 +0300

    fix: bug fixs - 9

--- api/server.js
+++ api/server.js
@@ -1,88 +1,32 @@
-const { readFileSync } = require("fs");
-const jsonServer = require("json-server");
-const path = require("path");
 
-const server = jsonServer.create();
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+const server = jsonServer.create()
 
-server.use(jsonServer.defaults({}));
-server.use(jsonServer.bodyParser);
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
 
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
 
-server.post("/login", (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const db = JSON.parse(
-      readFileSync(path.resolve(__dirname, "db.json"), "UTF-8")
-    );
-    const { users = [] } = db;
+const middlewares = jsonServer.defaults()
 
-    const userFromBd = users.find(
-      (user) => user.username === username && user.password === password
-    );
-
-    if (userFromBd) {
-      return res.json(userFromBd);
-    }
-
-    return res.status(403).json({ message: "User not found" });
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({ message: e.message });
-  }
-});
-
-// eslint-disable-next-line consistent-return
-server.use(async (req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(403).json({ message: "Auth error" });
-  }
-  next();
-});
-
-server.use(router);
-
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-  console.log("Server is running on 3000 port");
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
-
-// // See https://github.com/typicode/json-server#module
-// const jsonServer = require('json-server')
-
-// const server = jsonServer.create()
-
-// // Uncomment to allow write operations
-// // const fs = require('fs')
-// // const path = require('path')
-// // const filePath = path.join('db.json')
-// // const data = fs.readFileSync(filePath, "utf-8");
-// // const db = JSON.parse(data);
-// // const router = jsonServer.router(db)
-
-// // Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
-// const middlewares = jsonServer.defaults()
-
-// server.use(middlewares)
-// // Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
-// server.use(router)
-// server.listen(3000, () => {
-//     console.log('JSON Server is running')
-// })
-
-// Export the Server API
-// module.exports = server
+module.exports = server
